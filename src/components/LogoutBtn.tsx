e6ddc7bcf9f34481c6cf356c003cb4af86a07441
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-18 11:48:01 +0530

    mock up logout functionality

--- src/components/LogoutBtn.tsx
+++ src/components/LogoutBtn.tsx
@@ -2,12 +2,29 @@
 
 import React, { useState } from "react";
 import { Button } from "./ui/button";
+import { toast, Toaster } from "sonner";
+
 import { Loader2 } from "lucide-react";
+import { resolve } from "path";
+import { useRouter } from "next/navigation";
 
 function LogoutBtn() {
+  //   const toast = useToast();
+
   const [loading, setLoading] = useState(false);
-  const handleLogout = () => {
-    console.log("clicked");
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const errorMessage = null;
+    if (!errorMessage) {
+      toast.success("You have been logged out successfully");
+      router.push("/");
+    } else {
+      toast.error("logout failed");
+    }
+    setLoading(false);
   };
   return (
     <Button onClick={handleLogout} variant="outline" className="w-24">
